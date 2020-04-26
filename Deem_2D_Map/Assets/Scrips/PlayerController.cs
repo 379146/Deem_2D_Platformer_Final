c2f1dffee4405138359480fcb8743fcd2de219ba
379146 <379146@colum.edu>
2020-04-26 16:01:33 -0500

    First Push

---
+++ Deem_2D_Map/Assets/Scrips/PlayerController.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+
+[DisallowMultipleComponent]
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Collider2D))]
+
+
+public class PlayerController : MonoBehaviour
+{
+
+    [Header("Ground Movement")]
+    [Tooltip("Movement speed in tiles per second, one tile = one meter per second")]
+    [SerializeField]
+    private float speed;
+
+
+    [Header("Air Movement")]
+    [Tooltip("The upward force when a player jumps")]
+    [Range(0f,10f)]
+    [SerializeField]
+    private float jumpForce;
+
+
+
+    private Rigidbody2D playerRigidbody;
+    private bool isJumping;
+    private bool isFacingRight = true;
+
+    void Start()
+    {
+
+        playerRigidbody = GetComponent<Rigidbody2D>();
+
+
+
+    }
+
+
+
+
+
+    void Update()
+    {
+
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float xVelocity = horizontalInput * speed;
+        playerRigidbody.velocity = new Vector2(xVelocity, playerRigidbody.velocity.y);
+
+        if ((isFacingRight && horizontalInput < 0) || (!isFacingRight && horizontalInput > 0))
+        {
+
+            Flip();
+
+        }
+        
+
+
+
+
+    }
+
+
+    void FixedUpdate()
+    { 
+    
+        //to do put physics code in here not update 
+    
+    }
+
+
+    private void Flip()
+    {
+
+        isFacingRight = !isFacingRight;
+
+        Vector3 scale = transform.localScale;
+        scale.x = isFacingRight ? 1 : -1;
+        transform.localScale = scale;
+    
+    }
+
+}
